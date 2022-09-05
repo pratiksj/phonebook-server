c4420b725c887212d43485563937b33070af1fd7
pratiksj <pratiksharai728@gmail.com>
2022-09-05 22:43:56 +0545

    connected to database and error handled

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ App.get("/", (request, response) => {
   response.send("<h1>hellow world Niru</h1> "); // response.send response ko method of
 });
 
-App.get("/info", (request, response) => {
-  var currentdate = new Date();
-  response.send(
-    `Phonebook has information for ${persons.length} people <br/>${currentdate} `
-  ); // response.send response ko method of
-});
+// App.get("/info", (request, response) => {
+//   var currentdate = new Date();
+//   response.send(
+//     `Phonebook has information for ${persons.length} people <br/>${currentdate} `
+//   ); // response.send response ko method of
+// });
 
 App.get("/persons", (request, response) => {
 Person.find().then(result=>response.json(result))
@@ -44,41 +44,108 @@ Person.find().then(result=>response.json(result))
 });
 
 App.get("/persons/:id", (request, response) => {
-  const currendId = Number(request.params.id);
-  const thisNote = persons.find((person) => person.id === currendId);
-  if (thisNote) response.json(thisNote); // response.send response ko method of
-  else
-    response
-      .status(404)
-      .json({ error: 404, message: `There is no person with id ${currendId}` });
+  Person.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      next(error)
+      //response.status(500).end()
+      //response.status(400).send({ error: 'malformatted id' })
+    })
+  // const currendId = Number(request.params.id);
+  // const thisNote = Person.find((person) => person.id === currendId);
+  // if (thisNote) response.json(thisNote); // response.send response ko method of
+  // else
+  //   response
+  //     .status(404)
+  //     .json({ error: 404, message: `There is no person with id ${currendId}` });
 });
 
-App.delete("/persons/:id", (request, response) => {
-  const currendId = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== currendId);
-  //   const thisNote = persons.find((person) => person.id === currendId);
+App.delete('/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+// App.delete("/persons/:id", (request, response) => {
+//   const currendId = Number(request.params.id);
+//   Person = Person.filter((person) => person.id !== currendId);
+//   //   const thisNote = persons.find((person) => person.id === currendId);
 
-  response.status(204).end();
-});
+//   response.status(204).end();
+// });
 
-App.post("/persons/", (request, response) => {
-  let newData = request.body;
-  newData.id = Math.floor(Math.random() * 10000000);
-  let newPerson = persons.find((person) => person.name === newData.name);
-  if (newPerson) {
-    return response.status(400).json({ error: "Name must be unique" });
+// App.post("/persons/", (request, response) => {
+//   let newData = request.body;
+//   newData.id = Math.floor(Math.random() * 10000000);
+//   let newPerson = Person.find((person) => person.name === newData.name);
+//   if (newPerson) {
+//     return response.status(400).json({ error: "Name must be unique" });
+//   }
+//   if (
+//     newData.name === "" ||
+//     // newData.number === "" ||
+//     !newData.hasOwnProperty("name")
+//     // !newData.hasOwnProperty("number")
+//   ) {
+//     return response.status(400).json({ error: "Missing property" });
+//   }
+//   Person.push(newData);
+//   return response.status(201).json(newData);
+// });
+
+App.post('/persons/', (request, response) => {
+  const body = request.body
+
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
-  if (
-    newData.name === "" ||
-    // newData.number === "" ||
-    !newData.hasOwnProperty("name")
-    // !newData.hasOwnProperty("number")
-  ) {
-    return response.status(400).json({ error: "Missing property" });
+
+  const note = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+App.put('/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    name: body.name,
+    number: body.number,
   }
-  persons.push(newData);
-  return response.status(201).json(newData);
-});
+
+  Person.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+App.use(errorHandler)   //error ko middleware
+
 const PORT = process.env.PORT || "3001";
 
 App.listen(PORT, () => {
--- models/note.js
+++ models/note.js
@@ -14,16 +14,16 @@ mongoose.connect(url)
   })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  name: String,
   number: String
 })
 
-// noteSchema.set('toJSON', {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString()
-//     delete returnedObject._id
-//     delete returnedObject.__v
-//   }
-// })
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Person', noteSchema)
