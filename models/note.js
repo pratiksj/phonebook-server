9366f37aa8b5d5b6ec1e0531eaf337b1b64b3689
pratiksj <pratiksharai728@gmail.com>
2022-09-12 13:31:42 +0545

    amended the custom validation for number

--- models/note.js
+++ models/note.js
@@ -1,52 +1,60 @@
-const mongoose = require('mongoose')
-require("dotenv").config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log("connecting to", url);
 
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 const noteSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 5,
-    required: true
+    required: true,
   },
-  number:{
+  number: {
     type: String,
     validate: {
-        validator: function(v) {
-//console.log(v)
-            let splitedNum = v.split("-")
-            //console.log(splitedNum)
-            if(splitedNum.length===1&& v.length>=8){
-        return true
-            } else if (splitedNum[0].length===2||splitedNum[0].length===3){
-                return true 
-            } else {return false}
-          
-        },
-        message: props => `${props.value} is not a valid phone number!`
+      validator: function (v) {
+        //console.log(v);
+        let splitedNum = v.split("-");
+        //console.log("gf", splitedNum);
+        //console.log(splitedNum[0].length);
+        //if (splitedNum.length === 1 && v.length >= 8) {
+        if (v.length < 8) {
+          return false;
+        } else {
+          console.log(v.includes("-"));
+          if (!v.includes("-")) {
+            return true;
+          } else {
+            if (splitedNum[0].length !== 2 && splitedNum[0].length !== 3) {
+              return false;
+            }
+          }
+        }
       },
-  }
-    
-    
-   //String
-})
+      message: (props) => `${props.value} is not a valid phone numbers!`,
+    },
+  },
 
-noteSchema.set('toJSON', {
+  //String
+});
+
+noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Person', noteSchema)
+module.exports = mongoose.model("Person", noteSchema);
